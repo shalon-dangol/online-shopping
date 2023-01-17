89e2f33739a4cff4d684bcb967f90e1671568432
shalon_dangol <shalondangol.sd@gmail.com>
2023-01-17 23:06:03 +0545

    added

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+
+
+			<b>2022 PC Components Nepal </b> All rights reserved.
+		</div>
+	</div>
---
+++ includes/footer.php
@@ -0,0 +1,86 @@
+<footer id="footer" class="footer color-bg">
+    <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <!-- ============================================================= CONTACT INFO ============================================================= -->
+                    <div class="contact-info">
+                        <div class="footer-logo">
+                            <div class="logo">
+                                <a href="index.php">
+
+                                    <h3>PC Components Nepal</h3>
+                                </a>
+                            </div><!-- /.logo -->
+
+                        </div><!-- /.footer-logo -->
+
+                        <div class="module-body m-t-20">
+                            <p class="about-us"> PC Components Nepal is an innovative, leading computer hardware sales company situated in Kathmandu, Nepal. Remember us for all your computer hardware requirements for normal uses to heavy gaming uses. We provide a complete solution to all your hardware needs.</p>
+
+                            <div class="social-icons">
+
+                                <a href="#" class='active'><i class="icon fa fa-facebook"></i></a>
+                                <a href="#"><i class="icon fa fa-twitter"></i></a>
+                                <a href="#"><i class="icon fa fa-linkedin"></i></a>
+                            </div><!-- /.social-icons -->
+                        </div><!-- /.module-body -->
+
+                    </div><!-- /.contact-info -->
+                    <!-- ============================================================= CONTACT INFO : END ============================================================= -->
+                </div><!-- /.col -->
+
+
+
+                <div class="col-xs-12 col-sm-6 col-md-4" style="margin-Left: 350px;">
+                    <div class="contact-information">
+                        <div class="module-heading">
+                            <h4 class="module-title">information</h4>
+                        </div><!-- /.module-heading -->
+
+                        <div class="module-body outer-top-xs">
+                            <ul class="toggle-footer" style="">
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <p>Samakhusi, Kathmandu</p>
+                                    </div>
+                                </li>
+
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <p>+977-9883488383</p>
+                                    </div>
+                                </li>
+
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <span><a href="https://www.campcodes.com/">pc_components_nepal@gmail.com</a></span>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div><!-- /.module-body -->
+                    </div><!-- /.contact-timing -->
+                    <!-- ============================================================= INFORMATION : END ============================================================= -->
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.links-social -->
